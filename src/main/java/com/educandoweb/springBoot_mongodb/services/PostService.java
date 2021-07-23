4ba51ccd4c8808a0b3bf0219d9cf113e895558b6
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-23 12:33:17 -0300

    Obtendo um post por id

---
+++ src/main/java/com/educandoweb/springBoot_mongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.educandoweb.springBoot_mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.springBoot_mongodb.domain.Post;
+import com.educandoweb.springBoot_mongodb.repositories.PostRepository;
+import com.educandoweb.springBoot_mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
