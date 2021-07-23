50fd5eb6049b01b53e158782420af6a35990fa5e
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-23 15:32:09 -0300

    Consulta com vários critérios

--- src/main/java/com/educandoweb/springBoot_mongodb/repositories/PostRepository.java
+++ src/main/java/com/educandoweb/springBoot_mongodb/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.educandoweb.springBoot_mongodb.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,7 +13,10 @@ import com.educandoweb.springBoot_mongodb.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
-	
+
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
