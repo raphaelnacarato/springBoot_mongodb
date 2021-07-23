f9d954f71b1c78aada7635b96d0fa257fd13a1ec
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-23 12:29:38 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/educandoweb/springBoot_mongodb/resources/UserResource.java
+++ src/main/java/com/educandoweb/springBoot_mongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.educandoweb.springBoot_mongodb.domain.Post;
 import com.educandoweb.springBoot_mongodb.domain.User;
 import com.educandoweb.springBoot_mongodb.dto.UserDTO;
 import com.educandoweb.springBoot_mongodb.services.UserService;
@@ -68,6 +69,13 @@ public class UserResource {
 		obj = service.update(obj);
 
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
