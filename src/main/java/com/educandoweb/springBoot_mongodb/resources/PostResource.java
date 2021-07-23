e4eaa037c48a68d6195de7290fa95e4b5ea7eed6
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-23 15:03:25 -0300

    Consulta simples com query methods

--- src/main/java/com/educandoweb/springBoot_mongodb/resources/PostResource.java
+++ src/main/java/com/educandoweb/springBoot_mongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.springBoot_mongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.springBoot_mongodb.domain.Post;
+import com.educandoweb.springBoot_mongodb.resources.util.URL;
 import com.educandoweb.springBoot_mongodb.services.PostService;
 
 @RestController
@@ -24,4 +28,11 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+
+		return ResponseEntity.ok().body(list);
+	}
 }
